a8c8961118dfe4653be1e86e173e4ec4704d6d02
LeandroCarrizo <leandrocarrizo97@gmail.com>
2022-11-04 17:10:20 -0300

    Se agrega img_Cannon y se actualiza pantalla

--- new game.py
+++ new game.py
@@ -1,10 +1,16 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-venta = pygame.display.set_mode((400,300))
+ventana = pygame.display.set_mode((800,700))
 pygame.display.set_caption("Hola mundo")
 
+Cannon_img= pygame.image.load("img/cannon.webp")
+New_Cannon_img=pygame.transform.scale(Cannon_img,(int(100),int(100)))
+posX,posY=350,610
+
+
+ventana.blit(New_Cannon_img,(posX,posY))
 while True:
     for evento in pygame.event.get():
         if evento.type == QUIT :
