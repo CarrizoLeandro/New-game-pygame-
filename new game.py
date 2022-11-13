442007f4bedcf8f1a7181a462dd2ee89e3650578
LeandroCarrizo <leandrocarrizo97@gmail.com>
2022-11-13 00:10:05 -0300

    Se restructura el codigo por objetos

--- new game.py
+++ new game.py
@@ -1,47 +1,84 @@
 import pygame, sys
 from pygame.locals import *
+#Cariable Globales
+ancho= 900
+alto =480
+
+class naveEspacial(pygame.sprite.Sprite):
+    """Clase para las naves"""
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.ImagenNave= pygame.image.load("img/nave.jpg")
+
+        self.rect = self.ImagenNave.get_rect()
+        self.rect.centerx= ancho/2
+        self.rect.centery=alto-30
+
+        self.listaDisparo=[]
+        self.Vida= True
+
+        self.velocidad = 20
+
+    def movimiento(self):
+        if self.Vida == True:
+            if self.rect.left <= 0:
+                self.rect.left=0
+            elif self.rect.right > 870:
+                self.rect.right= 840
+        
+
+    def disparar(self):
+        print ("Disparo")
+    def dibujar (self, superficie):
+        superficie.blit(self.ImagenNave,self.rect)
+
+class proyectil (pygame.sprite.Sprite):
+    def __init__(self, posx,posy):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.imageProyectil = pygame.image.load("img/disparoa.jpg")
+        self.rect = self.imageProyectil.get_rect()
+
+        self.velocidadDisparo = 0.1
+
+        self.rect.top=posy
+        self.rect.left=posx
+    def trayectoria(self):
+        self.rect.top=self.rect.top - self.velocidadDisparo
+    def dibujar(self,superficie):
+        superficie.blit(self.imageProyectil,self.rect)
+
 #Inicio de ventana
-pygame.init()
-ventana = pygame.display.set_mode((800,700))
-pygame.display.set_caption("Hola mundo")
-#Fuentes
-aux=0
-Fuente=pygame.font.SysFont("Arial",30)
-Cannon_img= pygame.image.load("img/cannon.webp")
+def SpaceInvader(): 
+    pygame.init()
+    venta=pygame.display.set_mode((ancho,alto))
+    pygame.display.set_caption("Space Invader")
+    jugador= naveEspacial()
 
-#Se agrega Cañon
-New_Cannon_img=pygame.transform.scale(Cannon_img,(int(100),int(100)))
-posX,posY=350,610
+    DemoProyectil= proyectil(ancho/2, alto-30)
 
-#Velocidad
-velocidad=15
+    enJuego= True
 
+    while True:
+        #Eventos
+        venta.fill((0,0,0))
+        jugador.movimiento()
+        DemoProyectil.trayectoria()
+        for evento in pygame.event.get():
+            if evento.type == QUIT :
+                pygame.quit()
+                sys.exit()
+            if enJuego==True:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == K_LEFT:
+                        jugador.rect.left -= jugador.velocidad
+                    elif evento.key == K_RIGHT:
+                        jugador.rect.right += jugador.velocidad
+                    elif evento.key == K_BACKSPACE:
+                        jugador.disparar()
+        DemoProyectil.dibujar(venta)
+        jugador.dibujar(venta)
+        pygame.display.update()
 
-while True:
-    ventana.fill((225,225,225))
-    ventana.blit(New_Cannon_img,(posX,posY))
-    Tiempo=pygame.time.get_ticks()//1000
-    if aux==Tiempo:
-        aux+=1
-    #Eventos
-    for event in pygame.event.get():
-        if event.type == QUIT :
-            pygame.quit()
-            sys.exit()
-        elif event.type==pygame.KEYDOWN:
-            if event.key==K_LEFT:
-                posX-=velocidad
-            elif event.key==K_RIGHT:
-                posX+=velocidad
-
-    #Contador
-    contador=Fuente.render("Tiempo: "+str(Tiempo),0,(120,70,0))
-    ventana.blit(contador,(630,0))
-
-
-
-
-
-
-
-    pygame.display.update()
+SpaceInvader()
